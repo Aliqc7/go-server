5ebd1091001a13b224be890a1d41e673dbd51bb4
Ali <ali.zolghadr@gmail.com>
2024-02-03 18:10:34 +1100

    Add func helloHandler

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -14,5 +15,17 @@ func main() {
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
+
+}
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not Found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "GET request required!", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "Hello!")
 
 }
