fee3598191d33c40d8b7f3a02696e9a5655dd49b
Ali <ali.zolghadr@gmail.com>
2024-02-03 18:09:39 +1100

    Add func main

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+
+}
